5dfeb5f80c36a8ad34242f70699ea25bd35b6595
Ansh Laad <163443003+anshlaad@users.noreply.github.com>
2026-01-22 01:05:38 +0530

    Add files via upload

---
+++ orders/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Order
+
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ['first_name', 'last_name', 'phone', 'email', 'address_line_1', 'address_line_2', 'country', 'state', 'city', 'order_note']
+
+    """def __init__(self, arg):
+        super(OrderForm, self).__init__()
+        self.arg = arg
+        """
---
+++ orders/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('place_order/', views.place_order, name='place_order'),
+    path('payments/', views.payments, name='payments'),
+    path('order_complete/',  views.order_complete, name='order_complete'),
+    path('invoice/<str:order_number>/', views.download_invoice, name='download_invoice'),
+
+
+]
